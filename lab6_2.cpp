8068c235cec46bdd93ddfa51692f9ea29d4c7dca
6804062610215 <s6804062610215@email.kmutnb.ac.th>
2026-01-12 11:01:22 +0700

    lab6no1.c update array2d

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -8,10 +8,12 @@ int main()
 			scanf("%d", &t[i][j]);
 		}
 	}
-	printf("\n")
+	
+	printf("\n");
+	
 	for(i = 0;i < 3;i++){
 		for(j = 0;j < 4;j++){
-			printf("%d ", t[i][j]);
+			printf("%d ", t[i][j] + 1);
 		}
 		printf("\n");
 	}
