0b6c273e5966609ca5846c69cf630df21a45cf3c
6804062610215 <s6804062610215@email.kmutnb.ac.th>
2026-01-12 10:57:54 +0700

    Create lab6_2.cpp

---
+++ lab6_2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{ 
+	int i, j, t[3][4];
+	
+	for(i = 0;i < 3;i++){
+		for(j = 0;j < 4;j++){
+			scanf("%d", &t[i][j]);
+		}
+	}
+	printf("\n")
+	for(i = 0;i < 3;i++){
+		for(j = 0;j < 4;j++){
+			printf("%d ", t[i][j]);
+		}
+		printf("\n");
+	}
+}
